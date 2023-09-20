cf99a097d12d5b671c41bbd813549abe71d5ab8c
hamad0912 <38722133+hamad0912@users.noreply.github.com>
2023-09-20 13:30:41 +0300

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,2 @@
-<?php
-$db_host 		= "localhost"; # Database Host
-$db_user 		= 'root'; # Database Username
-$db_password 	= '1234'; # Database Password
-$db_name 		= 'ibilling'; # Database Name
-define('APP_URL', 'http://localhost/ibilling'); # Application URL.
-#Please include http and do not use trailing slash after the url. For example use in this format- http://www.example.com Or http://www.example.com/finance
-$_app_stage = 'Live'; # Change Live to Dev to enable Debug
+https://shifa-ph.com
+
